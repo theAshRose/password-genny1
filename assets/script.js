ade29891bbe6954fba9bf400cf4112a982fa5687
theDomconrad <them.and.us.2013@gmail.com>
2022-09-29 16:32:29 -0700

    added all notes

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,45 @@
-//Password generator!
-var charLowerCase = ["abcdefghijklmnopqrstuvwxyz"]; //Defining arrays as solid strings instead of an array of singular strings allows code to execute without including commas as array elements
+var charLowerCase = ["abcdefghijklmnopqrstuvwxyz"]; //Defining arrays as solid strings instead of an array of singular strings allows arrays to .concat without including commas as array elements
 var charUpperCase = ["ABCDEFGHIJKLMMOPQRSTUVWXYZ"]; //uppercase array
 var charNumeric = ["1234567890"];                   //number array
 var charSpecial = ["!@#$%^&*()_-+=<,>.?/"];         //special character array
-var generatedPassword = []; //<--------------------aggregate array
-var finalPassword = [];     //<--------------------aggregate array for randomized, trimmed elements
-var charLength = [];        //<--------------------length array defined by user and later used to define length of password
+var generatingPassword = []; //<---------------intital aggregate array for user input
+var finalPassword = [];     //<---------------final aggregate array for shuffled and trimmed user criteria
+var charLength = [];        //<---------------length array defined by user, used to define length of 'finalPassword'
 var generateBtn = document.querySelector("#generate"); //enables button on page
-generateBtn.addEventListener("click", writePassword);
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+generateBtn.addEventListener("click", writePassword); //starting gun for our app, initiates writePassword function --VERY important
+function writePassword() {   //<------------------------------function initiated upon our "click" event listener                         
+    var password = generatePassword();  //<-------------------defining a variable by initiating "generatePassword" function, after
+    var passwordText = document.querySelector("#password"); //takes the product of generatePassword function and prints it to designated location
+    passwordText.value = password;   //<----------------------converts value from generatePassword function
 }
-function generatePassword() {
-    charLength.length = prompt("Please enter a number for password length between 8-128 characters")
-    if (charLength.length < 8 || charLength.length > 128) {
-        alert("PLEASE RE-READ THE QUESTION, ANSWER APPROPRIATELY!");
-        generatePassword();
+function generatePassword() {  //Meat and potatoes of app where we gather all user data, combine it, shuffle and trim, then return
+    charLength.length = prompt("Please enter a number for password length between 8-128 characters")//defining variable to gather length of user password via prompt
+    if (charLength.length < 8 || charLength.length > 128) {     //<-----------------------------------returning boolean value as to whether length designated by user fits within certain peramiters
+        alert("PLEASE RE-READ THE QUESTION, ANSWER APPROPRIATELY!");    //<---------------------------if the 'if' statement returns TRUE an alert is displayed 
+        generatePassword();    //<--------------------------------------------------------------------and the function is called again so the user may enter accepted input
     };
-    var charLowerCaseOption = confirm("Press 'OK' if you'd like lowercase letters in your password! 'Cancel' if not");
-    if (charLowerCaseOption) {
-        generatedPassword += generatedPassword.concat(charLowerCase);
+    var charLowerCaseOption = confirm("Press 'OK' if you'd like lowercase letters in your password! 'Cancel' if not");//prompt asking user Y/N question to acceptance of a character type into their password
+    if (charLowerCaseOption) {                               //<--------------------------------------------------------if true, then..
+        generatingPassword += generatingPassword.concat(charLowerCase);  //<--------------------------------------------generatingPassword adds the designated array into itself using += and .concat
     }
     var charUpperCaseOption = confirm("Press 'OK' if you'd like UPPERCASE letters in your password! 'Cancel' if not");
     if (charUpperCaseOption) {
-        generatedPassword += generatedPassword.concat(charUpperCase);
+        generatingPassword += generatingPassword.concat(charUpperCase);
     }
     var charNumericOption = confirm("Press 'OK' if you'd like numbers(0-9) in your password! 'Cancel' if not");
     if (charNumericOption) {
-        generatedPassword += generatedPassword.concat(charNumeric);
+        generatingPassword += generatingPassword.concat(charNumeric);
     }
     var charSpecialOption = confirm("Press 'OK' if you'd like special(!@#$) letters in your password! 'Cancel' if not");
     if (charSpecialOption) {
-        generatedPassword += generatedPassword.concat(charSpecial);
+        generatingPassword += generatingPassword.concat(charSpecial);
     }
-    if (charLength.length === 0) {
-        alert("You messed up, pick at least one character type for your password, buddy!");
+    if (generatingPassword.length === 0) {                                                          //<--above if statements are ran until this final conditional
+        alert("You messed up, pick at least one character type for your password, buddy!");         //---where if true, generatePassword is run again - the user must start over
         generatePassword();
     }
-    for (var x = 0; x < charLength.length; x++) {
-        finalPassword = finalPassword + generatedPassword[Math.floor(Math.random() * generatedPassword.length)];
+    for (var x = 0; x < charLength.length; x++) { //-----For loop iterates through the subsequent math.random function for only the length of "charLength"                                      
+        finalPassword = finalPassword + generatingPassword[Math.floor(Math.random() * generatingPassword.length)]; //compiling random elements from 'generatedPassword' array into 'finalPassword' in accordance with previous for loop(designated length)
     }
-    return finalPassword;
+    return finalPassword; //finally generating our password and returning to the writePassword function, where it is converted into password -- > passwordText --> written into the document
 }
-
